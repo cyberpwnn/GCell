f2ee8f6c5d83fc2531d3341936cc98dff668e431
Dan <cyberpwnn@users.noreply.github.com>
2017-11-12 16:09:00 -0500

    Update wbv

--- src/main/java/org/cyberpwn/gcell/POI.java
+++ src/main/java/org/cyberpwn/gcell/POI.java
@@ -10,12 +10,12 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class POI
 {
-	public Workbook loadWorkbook(File f) throws IOException, InvalidFormatException
+	public static XSSFWorkbook loadXSSFWorkbook(File f) throws IOException, InvalidFormatException
 	{
 		return new XSSFWorkbook(f);
 	}
 
-	public void saveWorkbook(Workbook wb, File f) throws IOException
+	public static void saveWorkbook(Workbook wb, File f) throws IOException
 	{
 		FileOutputStream outputStream = new FileOutputStream(f);
 		wb.write(outputStream);
